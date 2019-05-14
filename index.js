8aac409ef78c0996f16f1a55cd2ca08977b6b2b5
Andrew Osheroff <andrewosh@gmail.com>
2019-05-14 13:30:13 +0200

    Added simple tests + bug fixes

--- index.js
+++ index.js
@@ -13,16 +13,24 @@ class RandomAccessMegastore extends EventEmitter {
     this.db = db
     this.networking = networking
     this.opts = opts
+
+    this._corestores = new Map()
+    this._ready = Promise.all([
+      this.networking.ready()
+    ])
   }
 
-  async get (name, coreOpts, opts = {}) {
+  ready () {
+    return this._ready
+  }
+
+  get (name, coreOpts, opts = {}) {
+    const self = this
+
     const storage = path => this.storage(name + '/' + path)
     const store = corestore(storage, { ...this.opts, coreOpts })
     const { get, replicate, close } = store
-    var encodedMainKey
-
-    // TODO: Support encrypted replication.
-    if (opts.seed !== false) this.networking.seed(store)
+    var mainKey, mainKeyString
 
     return {
       get: wrappedGet,
@@ -33,7 +41,7 @@ class RandomAccessMegastore extends EventEmitter {
     function wrappedClose (cb) {
       close(err => {
         if (err) return cb(err)
-        if (opts.seed !== false) this.networking.unseed(store)
+        if (opts.seed !== false) this.networking.unseed(mainKeyString)
       })
     }
 
@@ -46,9 +54,9 @@ class RandomAccessMegastore extends EventEmitter {
 
         const value = Core.encode({
           key: core.key,
-          seed: opts.seed,
+          seed: opts.seed !== false,
           writable: core.writable,
-          sparse: coreOpts.sparse,
+          sparse: !!coreOpts.sparse,
           valueEncoding: coreOpts.valueEncoding,
           name: coreOpts.name
         })
@@ -56,14 +64,21 @@ class RandomAccessMegastore extends EventEmitter {
         batch.push({ type: 'put', key: encodedKey, value })
         batch.push({ type: 'put', key: encodedDKey, value })
 
-        if (coreOpts.main) encodedMainKey = encodedKey
-        else batch.push({ type: 'put', key: encodedMainKey + '/' + encodedKey, value })
-        if (coreOpts.name) batch.push({ type: 'put', key: encodedMainKey + '/' + coreOpts.name, value })
+        if (coreOpts.main) {
+          mainKey = core.key
+          mainKeyString = encodedKey
+          self._corestores.set(encodedKey, store)
+          if (opts.seed !== false) self.networking.seed(core.discoveryKey, replicate)
+        } else {
+          batch.push({ type: 'put', key: mainKeyString + '/' + encodedKey, value })
+        }
+        if (coreOpts.name) batch.push({ type: 'put', key: mainKeyString + '/' + coreOpts.name, value })
 
-        this.db.batch(batch, err => {
+        self.db.batch(batch, err => {
           if (err) this.emit('error', err)
         })
       })
+      return core
     }
   }
 
@@ -84,7 +99,16 @@ class RandomAccessMegastore extends EventEmitter {
   }
 
   async close () {
-
+    return Promise.all([
+      this.networking.close(),
+      this.db.close(),
+      [...this._corestores].map((_, store) => new Promise((resolve, reject) => {
+        store.close(err => {
+          if (err) return reject(err)
+          return resolve()
+        })
+      }))
+    ])
   }
 }
 
---
+++ test/all.js
@@ -0,0 +1,47 @@
+const test = require('tape')
+
+const ram = require('random-access-memory')
+const memdb = require('memdb')
+
+const SwarmNetworker = require('megastore-swarm-networking')
+const Megastore = require('..')
+
+test('replication of two corestores', async t => {
+  const megastore1 = new Megastore(ram, memdb(), new SwarmNetworker({ port: 3006 }))
+  const megastore2 = new Megastore(ram, memdb(), new SwarmNetworker({ port: 3007 }))
+  await megastore1.ready()
+  await megastore2.ready()
+
+  const cs1 = megastore1.get('cs1')
+  const cs2 = megastore2.get('cs2')
+
+  const core1 = cs1.get({ main: true })
+  core1.ready(err => {
+    t.error(err, 'no error')
+    const core2 = cs2.get({ key: core1.key, main: true })
+    core2.ready(err => {
+      t.error(err, 'no error')
+      append(core1, core2)
+    })
+  })
+
+  function append(core1, core2) {
+    core1.append('hello', err => {
+      t.error(err, 'no error')
+      core2.get(0, (err, contents) => {
+        t.error(err, 'no error')
+        t.same(contents, Buffer.from('hello'))
+      })
+    })
+  }
+
+  await megastore1.close()
+  await megastore2.close()
+
+  t.end()
+})
+
+test('replication of two corestores with multiple channels')
+test('does not replicate a corestore that\'s not seeded')
+test('unseeds a replicating corestore')
+test('lists all corestores')
