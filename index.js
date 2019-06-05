a0d59939b89d6682a04704cbebb86b0b1b9e49bf
Andrew Osheroff <andrewosh@gmail.com>
2019-06-05 12:48:41 +0200

    Storage sharing + better close

--- index.js
+++ index.js
@@ -1,85 +1,171 @@
 const { EventEmitter } = require('events')
 const datEncoding = require('dat-encoding')
+const sub = require('subleveldown')
+const crypto = require('hypercore-crypto')
 const corestore = require('random-access-corestore')
+const collect = require('stream-collector')
 
 const { Core } = require('./lib/messages')
 
-class RandomAccessMegastore extends EventEmitter {
-  constructor(storage, db, networking, opts) {
+class Megastore extends EventEmitter {
+  constructor (storage, db, networking, opts) {
     if (typeof storage !== 'function') storage = path => storage(path)
     super()
-    this.opts = opts
 
+    this.opts = opts
     this.storage = storage
     this.db = db
     this.networking = networking
-    this.networking.on('error', err => this.emit('error', err))
+    if (this.networking) this.networking.on('error', err => this.emit('error', err))
+
+    this._coreIndex = sub(this.db, 'cores')
+    this._keyIndex = sub(this.db, 'keys')
+
+    // The megastore duplicates storage across corestore instances. This top-level cache allows us to
+    // reuse in-memory hypercores.
+    this._cores = new Map()
+    // Default feed keys are cached here so that they can be fetched synchronously. Created in _loadKeys.
+    this._keys = null
 
     this._corestores = new Map()
     this._ready = Promise.all([
-      this.networking.ready()
+      this._loadKeys(),
+      this.networking ? this.networking.ready() : Promise.resolve()
     ])
+  }
+
+  _loadKeys () {
+    return new Promise((resolve, reject) => {
+      collect(this._keyIndex.createReadStream(), (err, list) => {
+        if (err) return reject(err)
+        this._keys = new Map(list)
+        return resolve()
+      })
+    })
   }
 
   ready () {
     return this._ready
   }
 
-  get (name, coreOpts, opts = {}) {
+  get (name, opts = {}) {
     const self = this
+    var mainDiscoveryKey, mainKeyString
 
-    const storage = path => this.storage(name + '/' + path)
-    const store = corestore(storage, { ...this.opts, coreOpts })
-    const { get, replicate, close } = store
-    var mainKey, mainKeyString
+    const store = corestore(this.storage, { ...this.opts, ...opts })
+    const {
+      get: innerGet,
+      replicate: innerReplicate,
+      default: innerGetDefault,
+      list: innerList
+    } = store
 
     return {
+      default: wrappedDefault,
       get: wrappedGet,
-      close: wrappedClose,
-      replicate,
+      replicate: innerReplicate,
+      list: innerList,
+      close
     }
 
-    function wrappedClose (cb) {
-      close(err => {
-        if (err) return cb(err)
-        if (opts.seed !== false) this.networking.unseed(mainKeyString)
+    function getCore (getter, coreOpts) {
+      if (coreOpts && coreOpts.key) {
+        const existing = self._cores.get(datEncoding.encode(coreOpts.key))
+        if (existing) {
+          if (existing.refs.indexOf(name) === -1) existing.refs.push(name)
+          return existing.core
+        }
+      }
+      const core = getter(coreOpts)
+      core.on('ready', () => processCore(core, coreOpts))
+      return core
+    }
+
+    function processCore (core, coreOpts) {
+      const batch = []
+      const encodedKey = datEncoding.encode(core.key)
+      const encodedDiscoveryKey = datEncoding.encode(core.discoveryKey)
+
+      const value = Core.encode({
+        key: core.key,
+        seed: opts.seed !== false,
+        writable: core.writable,
+        sparse: !!coreOpts.sparse,
+        valueEncoding: coreOpts.valueEncoding
       })
+
+      batch.push({ type: 'put', key: encodedKey, value })
+      batch.push({ type: 'put', key: encodedDiscoveryKey, value })
+      self._cores.set(encodedKey, { core, refs: [name] })
+
+      if (coreOpts.default) {
+        mainDiscoveryKey = encodedDiscoveryKey
+        mainKeyString = encodedKey
+        self._corestores.set(mainKeyString, store)
+        if (opts.seed !== false && self.networking) self.networking.seed(core.discoveryKey, innerReplicate)
+      } else {
+        batch.push({ type: 'put', key: mainKeyString + '/' + encodedKey, value })
+      }
+
+      self._coreIndex.batch(batch, err => {
+        if (err) this.emit('error', err)
+      })
+    }
+
+    function close (cb) {
+      if (opts.seed !== false && mainDiscoveryKey && self.networking) self.networking.unseed(mainDiscoveryKey)
+
+      const cores = innerList()
+      var pending = 0
+
+      // TODO: How do we mark the corestore as closed when the close behavior is here?
+
+      for (let [key, core] of cores) {
+        const outerCore = self._cores.get(key)
+        if (!outerCore) continue
+
+        const { refs } = outerCore
+        refs.splice(refs.indexOf(name), 1)
+
+        if (!refs.length) {
+          pending++
+          core.close(err => {
+            if (err) return cb(err)
+            self._cores.delete(key)
+            if (!--pending) return onclosed()
+          })
+        }
+      }
+      if (!pending) return process.nextTick(onclosed)
+
+      function onclosed () {
+        self._corestores.delete(mainKeyString)
+        return cb(null)
+      }
+    }
+
+    function wrappedDefault (coreOpts) {
+      if (coreOpts instanceof Buffer) coreOpts = { key: coreOpts }
+
+      if (!coreOpts || !coreOpts.key) {
+        const keyPair = crypto.keyPair()
+        var { publicKey, secretKey } = keyPair
+        self._keys.set(name, keyPair)
+        self._keyIndex.put(name, secretKey, err => {
+          if (err) return self.emit('error', err)
+        })
+        coreOpts = { key: publicKey }
+      } else {
+        const storedKey = self._keys.get(name)
+        if (storedKey) secretKey = storedKey.secretKey
+      }
+      coreOpts = { ...coreOpts, secretKey, default: true }
+      return getCore(innerGetDefault, coreOpts)
     }
 
     function wrappedGet (coreOpts = {}) {
-      const core = get(coreOpts)
-      core.on('ready', () => {
-        const batch = []
-        const encodedKey = datEncoding.encode(core.key)
-        const encodedDKey = datEncoding.encode(core.discoveryKey)
-
-        const value = Core.encode({
-          key: core.key,
-          seed: opts.seed !== false,
-          writable: core.writable,
-          sparse: !!coreOpts.sparse,
-          valueEncoding: coreOpts.valueEncoding,
-          name: coreOpts.name
-        })
-
-        batch.push({ type: 'put', key: encodedKey, value })
-        batch.push({ type: 'put', key: encodedDKey, value })
-
-        if (coreOpts.main) {
-          mainKey = core.key
-          mainKeyString = encodedKey
-          self._corestores.set(encodedKey, store)
-          if (opts.seed !== false) self.networking.seed(core.discoveryKey, replicate)
-        } else {
-          batch.push({ type: 'put', key: mainKeyString + '/' + encodedKey, value })
-        }
-        if (coreOpts.name) batch.push({ type: 'put', key: mainKeyString + '/' + coreOpts.name, value })
-
-        self.db.batch(batch, err => {
-          if (err) this.emit('error', err)
-        })
-      })
-      return core
+      if (coreOpts instanceof Buffer) coreOpts = { key: coreOpts }
+      return getCore(innerGet, coreOpts)
     }
   }
 
@@ -101,7 +187,7 @@ class RandomAccessMegastore extends EventEmitter {
 
   async close () {
     return Promise.all([
-      this.networking.close(),
+      this.networking ? this.networking.close() : Promise.resolve(),
       this.db.close(),
       [...this._corestores].map(([, store]) => new Promise((resolve, reject) => {
         store.close(err => {
@@ -113,4 +199,4 @@ class RandomAccessMegastore extends EventEmitter {
   }
 }
 
-module.exports = RandomAccessMegastore
+module.exports = Megastore
--- test/all.js
+++ test/all.js
@@ -12,21 +12,24 @@ test('replication of two corestores', async t => {
   await megastore1.ready()
   await megastore2.ready()
 
+  megastore1.on('error', err => t.fail(err))
+  megastore2.on('error', err => t.fail(err))
+
   const cs1 = megastore1.get('cs1')
   const cs2 = megastore2.get('cs2')
 
   await new Promise(resolve => {
-    const core1 = cs1.get({ main: true })
+    const core1 = cs1.default()
     core1.ready(err => {
       t.error(err, 'no error')
-      const core2 = cs2.get({ key: core1.key, main: true })
+      const core2 = cs2.default({ key: core1.key })
       core2.ready(err => {
         t.error(err, 'no error')
         append(core1, core2)
       })
     })
 
-    function append(core1, core2) {
+    function append (core1, core2) {
       core1.append('hello', err => {
         t.error(err, 'no error')
         core2.get(0, (err, contents) => {
@@ -50,23 +53,26 @@ test('replication of two corestores with multiple channels', async t => {
   await megastore1.ready()
   await megastore2.ready()
 
+  megastore1.on('error', err => t.fail(err))
+  megastore2.on('error', err => t.fail(err))
+
   const cs1 = megastore1.get('cs1')
   const cs2 = megastore2.get('cs2')
 
   await new Promise(resolve => {
-    const core1 = cs1.get({ main: true })
-    const core2 = cs1.get({ name: 'core2' })
+    const core1 = cs1.default()
+    const core2 = cs1.get()
     core1.ready(err => {
       t.error(err, 'no error')
-      cs2.get({ key: core1.key, main: true })
-      const core3 = cs2.get({ key: core1.key })
-      core5.ready(err => {
+      cs2.default({ key: core1.key })
+      const core3 = cs2.get({ key: core2.key })
+      core3.ready(err => {
         t.error(err, 'no error')
-        append(core3, core5)
+        append(core2, core3)
       })
     })
 
-    function append(core1, core2) {
+    function append (core1, core2) {
       core1.append('hello', err => {
         t.error(err, 'no error')
         core2.get(0, (err, contents) => {
@@ -81,6 +87,94 @@ test('replication of two corestores with multiple channels', async t => {
   await megastore1.close()
   await megastore2.close()
 
+  t.end()
+})
+
+test('replicates across restarts', async t => {
+  const megastore1 = new Megastore(ram, memdb(), new SwarmNetworker({ port: 3006 }))
+  const megastore2 = new Megastore(ram, memdb(), new SwarmNetworker({ port: 3007 }))
+  var defaultKey
+
+  await megastore1.ready()
+  await megastore2.ready()
+  megastore1.on('error', err => t.fail(err))
+  megastore2.on('error', err => t.fail(err))
+
+  var cs1 = megastore1.get('cs1')
+  const cs2 = megastore2.get('cs2')
+
+  await createAndClose()
+  cs1 = megastore1.get('cs1')
+  await testAppend()
+
+  await megastore1.close()
+  await megastore2.close()
+
+  t.end()
+
+  function createAndClose () {
+    return new Promise(resolve => {
+      const defaultCore = cs1.default()
+      defaultCore.ready(err => {
+        t.error(err, 'no error')
+        defaultKey = defaultCore.key
+        cs1.close(err => {
+          t.error(err, 'no error')
+          return resolve()
+        })
+      })
+    })
+  }
+
+  function testAppend () {
+    return new Promise(resolve => {
+      const core1 = cs1.default(defaultKey)
+      core1.ready(err => {
+        t.error(err, 'no error')
+        const core2 = cs2.default({ key: core1.key })
+        core1.ready(err => {
+          t.error(err, 'no error')
+          append(core1, core2)
+        })
+      })
+
+      function append (core1, core2) {
+        core1.append('hello', err => {
+          t.error(err, 'no error')
+          core2.get(0, (err, contents) => {
+            t.error(err, 'no error')
+            t.same(contents, Buffer.from('hello'))
+            return resolve()
+          })
+        })
+      }
+    })
+  }
+})
+
+test('reuses hypercores across corestores', async t => {
+  const megastore = new Megastore(ram, memdb(), new SwarmNetworker({ port: 3006 }))
+  await megastore.ready()
+  megastore.on('error', err => t.fail(err))
+
+  const cs1 = megastore.get('cs1')
+  const cs2 = megastore.get('cs2')
+
+  const core1 = cs1.default()
+  await new Promise(resolve => {
+    core1.ready(err => {
+      t.error(err, 'no error')
+      const core2 = cs2.default(core1.key)
+      core2.ready(err => {
+        t.error(err, 'no error')
+        // core2 will only be writable if it's the same hypercore, since cs2 does not have access to its key.
+        t.true(core2.writable)
+        resolve()
+      })
+    })
+  })
+
+  await megastore.close()
   t.end()
 })
 
